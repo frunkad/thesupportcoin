906f37a153f39061a7dc268746edb5d87d0574eb
Darshan Baid <darshanbaid22@gmail.com>
2018-07-02 15:01:04 +0530

    borrow

--- src/app/borrow/borrow.component.ts
+++ src/app/borrow/borrow.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BorrowReqService } from '../borrow-req.service';
 import { AuthService } from '../auth.service';
+import {SuiModalService, TemplateModalConfig, ModalTemplate, SuiModal} from 'ng2-semantic-ui';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-borrow',
@@ -9,9 +11,33 @@ import { AuthService } from '../auth.service';
 })
 export class BorrowComponent implements OnInit {
 
-  constructor(public borrows: BorrowReqService, public auth: AuthService ) { }
+  newTaskForm = new FormGroup({
+    title: new FormControl()
+  });
+
+  @ViewChild("modalTemplate2")
+  public modalTemplate:ModalTemplate<{ data:string }, string, string>;
+
+  public dynamicContent:string = "Example of dynamic content.";
+
+
+  constructor(public borrows: BorrowReqService,public auth: AuthService,public modalService: SuiModalService) { }
 
   ngOnInit() {
   }
+  public open(dynamicContent:string = "Example"):void {
+    const config = new TemplateModalConfig<{ data:string }, string, string>(this.modalTemplate);
+
+    config.closeResult = "dismissed";
+    config.context = { data: dynamicContent };
+
+    this.modalService
+        .open(config)
+        .onApprove(r => {
+          this.borrows.createRequest(this.newTaskForm.value.title,"1030");
+        })
+        .onDeny(r => console.log(`Denied with result: '${r}'.`));
+}
 
 }
+
